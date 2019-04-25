cd87de922a2fb09a37420f49875b964961c492d7
Jake Thomas <jmaxt12@gmail.com>
2019-04-25 15:31:11 -0600

    "about me" and "endorsement" cards built out

---
+++ src/components/student/endorsements/Endorsements.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Endorsement = (props) => {
+    return ( 
+        <div>
+            <h2>Endorsement</h2>
+            <p>Endorsements from others</p>
+        </div>
+     );
+}
+ 
+export default Endorsement;
