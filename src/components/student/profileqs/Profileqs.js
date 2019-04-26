9b8b9bd3175f482fde6cdcd4f116e0c33ff02cda
Julian Moreno <julmo611@gmail.com>
2019-04-26 13:20:58 -0700

    adding more css to profile quic start

--- src/components/student/profileqs/Profileqs.js
+++ src/components/student/profileqs/Profileqs.js
@@ -5,32 +5,32 @@ import { connect } from 'react-redux';
 import ProfileqsForm from './ProfileqsForm';
 
 const Profileqs = props => {
-  const fakedInitialData = {
-    acclaimBadgeURL: '',
-    desiredTitle: 'Super Duper Developer',
-    gitHubURL: 'https://github.com',
-    linkedInURL: 'https://linkedin.com',
-    location: 'Ohio',
-    name: 'Bartholemew',
-    portfolioURL: '',
-    summary: 'hello, this is about',
-    twitterURL: ''
-  };
-  return (
-    <div className="profileqs-container">
-      <div className="profileqs">
-        <h3>Profile Quick Start</h3>
-        <p>Please complete the following basic information</p>
-        <ProfileqsForm initialData={fakedInitialData} />
-      </div>
-    </div>
-  );
+	const fakedInitialData = {
+		acclaimBadgeURL: '',
+		desiredTitle: 'Super Duper Developer',
+		gitHubURL: 'https://github.com',
+		linkedInURL: 'https://linkedin.com',
+		location: 'Ohio',
+		name: 'Bartholemew',
+		portfolioURL: '',
+		summary: 'hello, this is about',
+		twitterURL: ''
+	};
+	return (
+		<div className="profileqs-container">
+			<div className="profileqs">
+				<h3>Profile Quick Start</h3>
+				<p>Please complete the following basic information</p>
+				<ProfileqsForm initialData={fakedInitialData} />
+			</div>
+		</div>
+	);
 };
 
 const mapStateToProps = state => {
-  return {
-    ...state.profileqs
-  };
+	return {
+		...state.profileqs
+	};
 };
 
 export default connect(mapStateToProps)(Profileqs);
