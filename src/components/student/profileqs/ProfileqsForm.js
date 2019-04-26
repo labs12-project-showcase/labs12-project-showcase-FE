81640e46dbf5e46192127801d0774e177583e426
Clark <rclark010@gmail.com>
2019-04-26 13:59:00 -0600

    PUT request to back end successful

--- src/components/student/profileqs/ProfileqsForm.js
+++ src/components/student/profileqs/ProfileqsForm.js
@@ -2,6 +2,57 @@ import React from 'react';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
 
+// faked import of axiosAuth()
+import axios from 'axios';
+function axiosAuth() {
+  const token = localStorage.getItem('backendToken');
+
+  return axios.create({
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `${token}`
+    }
+  });
+}
+
+// Handle Form Submit, `PUT` request
+const handleSubmit = values => {
+  const backEndURL = 'https://halg-backend.herokuapp.com/api/students/update';
+  
+  // match Object keys to what back end expects
+  const formattedObj = {
+    student: {
+      about: values.summary,
+      acclaim: values.acclaimBadgeURL,
+      // desired_title: values.desiredTitle,
+      github: values.gitHubURL,
+      linkedin: values.linkedInURL,
+      location: values.location,
+      twitter: values.twitterURL,
+      website: values.portfolioURL
+    }
+  };
+
+  // Remove empty strings from Object
+  const removeEmpty = obj =>
+    Object.keys(obj)
+      .filter(f => Boolean(obj[f]) !== false)
+      .reduce(
+        (r, i) =>
+          typeof obj[i] === 'object'
+            ? { ...r, [i]: removeEmpty(obj[i]) } // recurse if nested Object
+            : { ...r, [i]: obj[i] },
+        {}
+      );
+
+  console.log('format strip obj: ', removeEmpty(formattedObj));
+
+  axiosAuth()
+    .put(backEndURL, removeEmpty(formattedObj))
+    .then(result => console.log('successful POST request – result: ', result))
+    .catch(error => console.error(error));
+};
+
 // Render the Formik form
 const renderForm = ({ errors, status, touched, isSubmitting }) => (
   <Form className="profile-quick-start-form">
@@ -79,25 +130,25 @@ const renderForm = ({ errors, status, touched, isSubmitting }) => (
 // Validation Schema, feels similar to React PropTypes
 const ProfileQsSchema = Yup.object().shape({
   acclaimBadgeURL: Yup.string()
-  .trim()
-  .url('Must be a valid URL'),
+    .trim()
+    .url('Must be a valid URL'),
   desiredTitle: Yup.string()
-  .max(100, `Maximum 100 characters`)
-  .trim('Must be a valid URL'),
+    .max(100, `Maximum 100 characters`)
+    .trim('Must be a valid URL'),
   gitHubURL: Yup.string()
-  .trim()
-  .url('Must be a valid URL'),
+    .trim()
+    .url('Must be a valid URL'),
   linkedInURL: Yup.string()
-  .trim()
-  .url('Must be a valid URL'),
+    .trim()
+    .url('Must be a valid URL'),
   location: Yup.string().trim(),
   name: Yup.string()
-  .max(100, `Maximum 100 characters`)
-  .required('Name is required')
-  .trim('Must be a valid URL'),
+    .max(100, `Maximum 100 characters`)
+    .required('Name is required')
+    .trim('Must be a valid URL'),
   portfolioURL: Yup.string()
-  .trim()
-  .url('Must be a valid URL'),
+    .trim()
+    .url('Must be a valid URL'),
   summary: Yup.string()
     .max(1000, `Maximum 1,000 characters`)
     .trim(),
@@ -124,13 +175,14 @@ const ProfileqsForm = props => {
     <Formik
       initialValues={initialFormValues}
       onSubmit={
+        handleSubmit
         // replace testing function with data submit function
-        (values, actions) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            actions.setSubmitting(false);
-          }, 1000);
-        }
+        // (values, actions) => {
+        //   setTimeout(() => {
+        //     alert(JSON.stringify(values, null, 2));
+        //     actions.setSubmitting(false);
+        //   }, 1000);
+        // }
       }
       validationSchema={ProfileQsSchema}
       render={renderForm}
