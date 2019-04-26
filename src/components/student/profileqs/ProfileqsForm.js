84027d9b4d0977abe75cdd7c8bb9d8e020604eae
Clark <rclark010@gmail.com>
2019-04-25 23:14:13 -0600

    Minor refactoring

--- src/components/student/profileqs/ProfileqsForm.js
+++ src/components/student/profileqs/ProfileqsForm.js
@@ -2,6 +2,80 @@ import React from 'react';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
 
+// Render the Formik form
+const renderForm = ({ errors, status, touched, isSubmitting }) => (
+  <Form className="profile-quick-start-form">
+    <label>
+      <span className="input-label">Name</span>
+      <br />
+      <Field name="name" type="text" />
+      <ErrorMessage name="name" component="div" />
+    </label>
+
+    <label>
+      <span className="input-label">Desired Title</span>
+      <br />
+      <Field name="desiredTitle" type="text" />
+      <ErrorMessage name="desiredTitle" component="div" />
+    </label>
+
+    <label>
+      <span className="input-label">Location</span>
+      <br />
+      <Field name="location" type="text" />
+      <ErrorMessage name="location" component="div" />
+    </label>
+
+    <label>
+      <span className="input-label">Portfolio URL</span>
+      <br />
+      <Field name="portfolioURL" type="text" />
+      <ErrorMessage name="portfolioURL" component="div" />
+    </label>
+
+    <label>
+      <span className="input-label">GitHub Profile URL</span>
+      <br />
+      <Field name="gitHubURL" type="text" />
+      <ErrorMessage name="gitHubURL" component="div" />
+    </label>
+
+    <label>
+      <span className="input-label">LinkedIn Profile URL</span>
+      <br />
+      <Field name="linkedInURL" type="text" />
+      <ErrorMessage name="linkedInURL" component="div" />
+    </label>
+
+    <label>
+      <span className="input-label">Twitter Profile URL</span>
+      <br />
+      <Field type="text" name="twitterURL" />
+      <ErrorMessage name="twitterURL" component="div" />
+    </label>
+
+    <label>
+      <span className="input-label">Acclaim Badge URL</span>
+      <br />
+      <Field name="acclaimBadgeURL" type="text" />
+      <ErrorMessage name="acclaimBadgeURL" component="div" />
+    </label>
+
+    <label className="stretch-input">
+      Tell prospective employers about yourself (500 words)
+      <br />
+      <span className="input-label">Summary</span>
+      <br />
+      <Field name="about" type="text" />
+      <ErrorMessage name="about" component="div" />
+    </label>
+
+    <button type="submit" disabled={isSubmitting}>
+      Create Profile
+    </button>
+  </Form>
+);
+
 // Validation Schema, feels similar to React PropTypes
 const ProfileQsSchema = Yup.object().shape({
   about: Yup.string()
@@ -45,6 +119,7 @@ const ProfileqsForm = props => {
     summary: props.initialData.summary || '',
     twitterURL: props.initialData.twitterURL || ''
   };
+
   return (
     <Formik
       initialValues={initialFormValues}
@@ -58,78 +133,7 @@ const ProfileqsForm = props => {
         }
       }
       validationSchema={ProfileQsSchema}
-      render={({ errors, status, touched, isSubmitting }) => (
-        <Form className="profile-quick-start-form">
-          <label>
-            <span className="input-label">Name</span>
-            <br />
-            <Field name="name" type="text" />
-            <ErrorMessage name="name" component="div" />
-          </label>
-
-          <label>
-            <span className="input-label">Desired Title</span>
-            <br />
-            <Field name="desiredTitle" type="text" />
-            <ErrorMessage name="desiredTitle" component="div" />
-          </label>
-
-          <label>
-            <span className="input-label">Location</span>
-            <br />
-            <Field name="location" type="text" />
-            <ErrorMessage name="location" component="div" />
-          </label>
-
-          <label>
-            <span className="input-label">Portfolio URL</span>
-            <br />
-            <Field name="portfolioURL" type="text" />
-            <ErrorMessage name="portfolioURL" component="div" />
-          </label>
-
-          <label>
-            <span className="input-label">GitHub Profile URL</span>
-            <br />
-            <Field name="gitHubURL" type="text" />
-            <ErrorMessage name="gitHubURL" component="div" />
-          </label>
-
-          <label>
-            <span className="input-label">LinkedIn Profile URL</span>
-            <br />
-            <Field name="linkedInURL" type="text" />
-            <ErrorMessage name="linkedInURL" component="div" />
-          </label>
-
-          <label>
-            <span className="input-label">Twitter Profile URL</span>
-            <br />
-            <Field type="text" name="twitterURL" />
-            <ErrorMessage name="twitterURL" component="div" />
-          </label>
-
-          <label>
-            <span className="input-label">Acclaim Badge URL</span>
-            <br />
-            <Field name="acclaimBadgeURL" type="text" />
-            <ErrorMessage name="acclaimBadgeURL" component="div" />
-          </label>
-
-          <label className="stretch-input">
-            Tell prospective employers about yourself (500 words)
-            <br />
-            <span className="input-label">Summary</span>
-            <br />
-            <Field name="about" type="text" />
-            <ErrorMessage name="about" component="div" />
-          </label>
-
-          <button type="submit" disabled={isSubmitting}>
-            Create Profile
-          </button>
-        </Form>
-      )}
+      render={renderForm}
     />
   );
 };
