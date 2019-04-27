68121f6f13d91cf2f66d562c1c0652d5fc66ea63
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-26 19:11:17 -0500

    Adds temporary fix for profile creation populating

--- src/components/student/profileqs/Profileqs.js
+++ src/components/student/profileqs/Profileqs.js
@@ -1,27 +1,47 @@
 //Profile Quick Start
 
-import React from 'react';
-import { connect } from 'react-redux';
-import ProfileqsForm from './ProfileqsForm';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { connect } from "react-redux";
+import ProfileqsForm from "./ProfileqsForm";
 
 const Profileqs = props => {
+  const [data, updateData] = useState({});
+
+  useEffect(() => {
+    const headers = {
+      authorization: localStorage.getItem("backendToken")
+    };
+    axios
+      .get("https://halg-backend.herokuapp.com/api/auth/login/initial", {
+        headers
+      })
+      .then(({ data }) => {
+        console.log(data);
+        updateData(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
   const fakedInitialData = {
-    acclaimBadgeURL: '',
-    desiredTitle: 'Super Duper Developer',
-    gitHubURL: 'https://github.com',
-    linkedInURL: 'https://linkedin.com',
-    location: 'Ohio',
-    name: 'Bartholemew',
-    portfolioURL: '',
-    summary: 'hello, this is about',
-    twitterURL: ''
+    acclaimBadgeURL: "",
+    desiredTitle: "Super Duper Developer",
+    gitHubURL: "https://github.com",
+    linkedInURL: "https://linkedin.com",
+    location: "Ohio",
+    name: "Bartholemew",
+    portfolioURL: "",
+    summary: "hello, this is about",
+    twitterURL: ""
   };
   return (
     <div className="profileqs-container">
       <div className="profileqs">
         <h3>Profile Quick Start</h3>
         <p>Please complete the following basic information</p>
-        <ProfileqsForm initialData={fakedInitialData} />
+        <ProfileqsForm initialData={data} />
       </div>
     </div>
   );
