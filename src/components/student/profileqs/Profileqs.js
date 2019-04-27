1807bbf695eb7b4707d55cfc7061d64051df39f7
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-26 19:12:10 -0500

    Pushes a file I did not save

--- src/components/student/profileqs/Profileqs.js
+++ src/components/student/profileqs/Profileqs.js
@@ -6,42 +6,12 @@ import { connect } from "react-redux";
 import ProfileqsForm from "./ProfileqsForm";
 
 const Profileqs = props => {
-  const [data, updateData] = useState({});
-
-  useEffect(() => {
-    const headers = {
-      authorization: localStorage.getItem("backendToken")
-    };
-    axios
-      .get("https://halg-backend.herokuapp.com/api/auth/login/initial", {
-        headers
-      })
-      .then(({ data }) => {
-        console.log(data);
-        updateData(data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }, []);
-
-  const fakedInitialData = {
-    acclaimBadgeURL: "",
-    desiredTitle: "Super Duper Developer",
-    gitHubURL: "https://github.com",
-    linkedInURL: "https://linkedin.com",
-    location: "Ohio",
-    name: "Bartholemew",
-    portfolioURL: "",
-    summary: "hello, this is about",
-    twitterURL: ""
-  };
   return (
     <div className="profileqs-container">
       <div className="profileqs">
         <h3>Profile Quick Start</h3>
         <p>Please complete the following basic information</p>
-        <ProfileqsForm initialData={data} />
+        <ProfileqsForm />
       </div>
     </div>
   );
