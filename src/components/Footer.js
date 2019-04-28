59dd739caa8e61ed7b1a2b2c05351271a947b587
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-28 00:34:23 -0400

    Adds links to <Footer /> to emulate ACTUAL lambdaschool.com footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,44 @@ export default function Footer() {
 	return (
 		<>
 			<footer>
+				<nav>
+					<NavLink to="/">
+						COURSES
+					</NavLink>
+					<NavLink to="/">
+						ABOUT
+					</NavLink>
+					<NavLink to="/">
+						BLOG
+					</NavLink>
+					<NavLink to="/">
+						APPLY
+					</NavLink>
+					<NavLink to="/">
+						CAREERS
+					</NavLink>
+					<NavLink to="/">
+						LEGAL
+					</NavLink>
+					<NavLink to="/">
+						CONTACT
+					</NavLink>
+					<NavLink to="/">
+						FREE CODE BOOTCAMP 
+					</NavLink>
+					<NavLink to="/">
+						REFERRAL
+					</NavLink>
+					<NavLink to="/">
+						<i className="fab fa-twitter"></i>
+					</NavLink>
+					<NavLink to="/">
+						<i className="fab fa-facebook"></i>
+					</NavLink>
+					<NavLink to="/">
+						<i className="fab fa-github"></i>
+					</NavLink>
+				</nav>
 				<NavLink exact to="/">
 					<img
 						className="TopBar-hire-lambda-logo"
