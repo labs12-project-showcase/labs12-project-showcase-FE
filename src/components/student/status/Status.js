93120dad0a5e17247b4a34b6aafa0eb05536b995
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-29 10:58:12 -0500

    Changes status section to use 'mock' data

--- src/components/student/status/Status.js
+++ src/components/student/status/Status.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React from "react";
 
-const Status = (props) => {
-    return ( 
-        <div>
-            <h1>Status</h1>
-            <p>Graduate of:</p>
-            <p>Looking for Work in:</p>
-            <p>Looking for:</p>
-        </div>
-     );
-}
- 
+const Status = ({ cohort, desired_locations, desired_title }) => {
+  //TEST DATA
+  cohort = "Full Stack Web17";
+  desired_locations = ["Houston, Tx", "San Diego, Ca", "NYC, NY"];
+  desired_title = "Software Engineer";
+
+  return (
+    <div className="status">
+      <h1>Status</h1>
+      <div className="status-section">
+        <p>Graduate of:</p>
+        <p>{cohort}</p>
+      </div>
+      <div className="status-section">
+        <p>Looking for Work in:</p>
+        <ul>
+          {desired_locations.map(loc => (
+            <li>{loc}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="status-section">
+        <p>Looking for:</p>
+        <p>{desired_title}</p>
+      </div>
+    </div>
+  );
+};
+
 export default Status;
