6a3436f84ee2f6eca23d9ee9af02375c600bac34
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-29 08:54:38 -0500

    Adds not endorsed banner

---
+++ src/components/student/notEndorsed/NotEndorsed.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./_notEndorsed.scss";
+
+const NotEndorsed = () => {
+  return (
+    <div className="endorsement-banner">
+      A career coach has not endorsed you yet. Your profile will not be public
+      until you have been approved.
+    </div>
+  );
+};
+
+export default NotEndorsed;
