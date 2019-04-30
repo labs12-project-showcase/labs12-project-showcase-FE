b4be96f47169da3e2fe43380654dc1c4dc33c0d4
andrewlowell <andrewlowellr@gmail.com>
2019-04-30 10:02:05 -0600

    Get pre-population on the quickstart profile form working and redirect to the profile page, pull and merge with master.
    Co-authored-by: rclark010@gmail.com

--- src/components/student/profileqs/Profileqs.js
+++ src/components/student/profileqs/Profileqs.js
@@ -10,32 +10,13 @@ const Profileqs = props => {
     props.getProfileData();
   }, []);
 
-  // const [initialData, setInitialData] = useState(
-	// 	props.profile.data
-	// // 	{
-  // //   about: props.profile.profileData.about || '',
-  // //   acclaim: props.profile.profileData.acclaim || '',
-  // //   desired_locations: props.profile.profileData.desired_locations || [],
-  // //   desired_title: props.profile.profileData.desired_title || '',
-  // //   github: props.profile.profileData.github || '',
-  // //   hobbies: props.profile.profileData.hobbies || [],
-  // //   linkedin: props.profile.profileData.linkedin || '',
-  // //   location: props.profile.profileData.location || '',
-  // //   name: props.profile.profileData.name || '',
-  // //   profile_pic: props.profile.profileData.profile_pic || '',
-  // //   skills: props.profile.profileData.skills || [],
-  // //   twitter: props.profile.profileData.twitter || '',
-  // //   website: props.profile.profileData.website || ''
-	// // }
-	// );
-
   return (
     <div className="profileqs-container">
       <div className="profileqs">
         {/* @TODO: Make the `document.title` and <h3> dynamic */}
         <h3>Profile Quick Start</h3>
         <p>Please complete the following basic information</p>
-        <ProfileqsForm initialFormValues={props.profile.data} />
+        <ProfileqsForm initialFormValues={props.profile.profileData} />
       </div>
     </div>
   );
