ec88cf2147aa3a7ac8dcdb5916a78bd10c57178e
Clark <rclark010@gmail.com>
2019-04-29 18:08:25 -0600

    Adds Profile reducer

--- src/config/rootReducer.js
+++ src/config/rootReducer.js
@@ -4,10 +4,12 @@ import homeReducer from '../components/home/homeReducer.js';
 import registerReducer from '../components/register/registerReducer.js';
 import adminDashboardReducer from '../components/admin/dashboard/adminDashboardReducer.js';
 import studentDashboardReducer from '../components/student/dashboard/studentDashboardReducer.js';
+import profileReducer from '../components/student/profileqs/profileqsReducer';
 
 export default combineReducers({
-  home: homeReducer,
-  register: registerReducer,
   adminDashboard: adminDashboardReducer,
+  home: homeReducer,
+  profile: profileReducer,
+  register: registerReducer,
   studentDashboard: studentDashboardReducer
 });
