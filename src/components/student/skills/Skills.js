98c20e7ee34b7066a90a56f640dc477b1a742a33
Julian Moreno <julmo611@gmail.com>
2019-04-30 14:13:05 -0700

    css changes for mobile and web, project page

--- src/components/student/skills/Skills.js
+++ src/components/student/skills/Skills.js
@@ -1,30 +1,32 @@
 import React from 'react';
 
 const Skills = ({ top_skills, skills }) => {
-	// top_skills = ["React", "Redux", "Node.js"];
-	// skills = [
-	//   "MongoDB",
-	//   "SQL",
-	//   "OOP",
-	//   "HTML5",
-	//   "CSS3",
-	//   "JavaScript",
-	//   "Python",
-	//   "C++",
-	//   "SASS",
-	//   "LESS"
-	// ];
+	top_skills = ['React', 'Redux', 'Node.js'];
+	skills = [
+		'MongoDB',
+		'SQL',
+		'OOP',
+		'HTML5',
+		'CSS3',
+		'JavaScript',
+		'Python',
+		'C++',
+		'SASS',
+		'LESS'
+	];
+	const map = arr =>
+		arr.map(skill => <span className="skill-chip">{skill}</span>);
 
 	return (
 		<div className="skills-container">
 			<h2>Skills</h2>
 			<section className="top-skills">
 				<h3>Top Three:</h3>
-				<ul>{top_skills}</ul>
+				<ul>{map(top_skills)}</ul>
 			</section>
 			<section className="skills">
 				<h3>Additional:</h3>
-				<ul>{skills}</ul>
+				<ul>{map(skills)}</ul>
 			</section>
 		</div>
 	);
