8a97656f7b89b7876868ca8208bfc3814ef1aebc
Jake Thomas <jmaxt12@gmail.com>
2019-05-01 17:37:17 -0600

    added imports for react-table

--- src/components/admin/dashboard/AdminDashboard.js
+++ src/components/admin/dashboard/AdminDashboard.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css'
+
 
 const AdminDashboard = props => {
   return (
