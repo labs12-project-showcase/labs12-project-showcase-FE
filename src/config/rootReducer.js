25857479b40d3fee070608255c1a9ae265b4c3b4
Jake Thomas <jmaxt12@gmail.com>
2019-05-01 14:10:07 -0600

    Changed reference from dashboard to profile

--- src/config/rootReducer.js
+++ src/config/rootReducer.js
@@ -4,7 +4,7 @@ import authReducer from '../auth/authReducer.js';
 import homeReducer from '../components/home/homeReducer.js';
 import registerReducer from '../components/register/registerReducer.js';
 import adminDashboardReducer from '../components/admin/dashboard/adminDashboardReducer.js';
-import studentDashboardReducer from '../components/student/dashboard/studentDashboardReducer.js';
+import studentProfileReducer from '../components/student/profile/studentProfileReducer.js';
 import profileReducer from '../components/student/profileqs/profileqsReducer';
 
 export default combineReducers({
@@ -13,5 +13,5 @@ export default combineReducers({
   home: homeReducer,
   profile: profileReducer,
   register: registerReducer,
-  studentDashboard: studentDashboardReducer
+  studentProfile: studentProfileReducer
 });
