adc294cfc219aabdf7424d4bafa637402dcbfb0b
andrewlowell <andrewlowellr@gmail.com>
2019-05-01 10:12:01 -0600

    Change the top bar to use better logic to determine whether or not a user is logged in, and add the subnav header thingy that should be on every page if the user is logged in.

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import history from '../history.js';
 import { connect } from 'react-redux';
 import { login, logout } from '../auth/authActions.js';
 import { NavLink } from 'react-router-dom';
@@ -9,6 +10,23 @@ import logoutIcon from '../assets/logout.png';
 import whiteLambdaLogo from '../assets/Hire-lambda-logo-white.png';
 
 class TopBar extends Component {
+	state = {
+		id: 0,
+		isLoggedIn: false
+	}
+
+	componentWillMount() {
+		this.unlisten = history.listen((location, action) => {
+			console.log('history from top bar', location, action);
+			const update = validateJwt();
+			this.setState({ isLoggedIn: update });
+		});
+	}
+
+	componentWillUnmount() {
+		this.unlisten();
+	}
+
 	render() {
 		const { login, logout } = this.props;
 		return (
@@ -23,18 +41,33 @@ class TopBar extends Component {
 					</NavLink>
 	
 					<div className="TopBar-btn-container">
-						{!validateJwt() && (
+						{!this.state.isLoggedIn && (
 							<button className="TopBar-login-btn" onClick={login}>
 								<img src={loginIcon} alt="Logout icon" />
 							</button>
 						)}
-						{validateJwt() && (
+						{this.state.isLoggedIn && (
 							<button className="TopBar-logout-btn" onClick={logout}>
 								<img src={logoutIcon} alt="Logout icon" />
 							</button>
 						)}
 					</div>
 				</div>
+				{this.state.isLoggedIn && (
+					<div className="subNav">
+						<nav>
+							<NavLink exact to={`/student/dashboard/${this.state.id}`}>
+
+							</NavLink>
+							<NavLink exact to="/student/new-project">
+								Add New Project
+							</NavLink>
+							<NavLink exact to="#">
+								Edit Profile
+							</NavLink>
+						</nav>
+					</div>
+				)}
 			</div>
 		);
 	}
