2c920fdee3d19c19ac0f2fdc7449b621a2b7eaf4
Clark <rclark010@gmail.com>
2019-05-01 20:33:53 -0600

    Prop drilling for SkillsList

--- src/components/student/profileqs/ProfileqsForm.js
+++ src/components/student/profileqs/ProfileqsForm.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Formik } from 'formik';
 
 import { ProfileQsSchema, FormSchema } from './ProfileqsFormSchema';
 import { updateProfile } from './profileqsActions';
 
+const ProfileqsForm = ({ dispatch, ...props }) => {
+  const [formSkillsList, setFormSkillsList] = useState([]);
 
-const ProfileqsForm = ({ dispatch, ...props}) => {
   return (
     <Formik
-			initialValues={props.initialFormValues}
-      onSubmit={(values) => dispatch(updateProfile(values))}
+      initialValues={props.initialFormValues}
+      onSubmit={values =>
+        dispatch(
+          updateProfile({
+            ...values,
+            skills: formSkillsList.map(item => item.value)
+          })
+        )
+      }
       validationSchema={ProfileQsSchema}
       enableReinitialize
-      render={props => <FormSchema {...props} />}
+      render={props => (
+        <FormSchema
+          skillsList={formSkillsList}
+          setSkillsList={setFormSkillsList}
+          {...props}
+        />
+      )}
     />
   );
 };
