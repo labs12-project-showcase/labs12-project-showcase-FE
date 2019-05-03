b3c1065b30101422e047577b508f6bd4a526463b
Julian Moreno <julmo611@gmail.com>
2019-05-02 17:53:22 -0700

    New carousel images

--- src/components/home/Carousel.js
+++ src/components/home/Carousel.js
@@ -1,120 +1,85 @@
-import React from "react";
-import Swiper from "react-id-swiper/lib/ReactIdSwiper.full";
+import React from 'react';
+import Swiper from 'react-id-swiper/lib/ReactIdSwiper.full';
 
 const ProjectViewHome = ({ projects_shocase }) => {
-  projects_shocase = [
-    {
-      name: "Reel Viewer",
-      imageUrl:
-        "https://www.uncommongoods.com/images/items/47400/47435_1_360px.jpg"
-    },
-    {
-      name: "Mathematical Glasses - Set of 4",
+	projects_shocase = [
+		{
+			name: 'Mentors International',
+			imageUrl: 'https://morenowtech.com/wp-content/uploads/2019/05/tico.jpg'
+		},
+		{
+			name: 'Fit Me',
+			imageUrl: 'https://morenowtech.com/wp-content/uploads/2019/05/fit-me.jpg'
+		},
+		{
+			name: 'Tabless Thursday',
+			imageUrl:
+				'https://morenowtech.com/wp-content/uploads/2019/05/tabless-thursday.jpg'
+		},
+		{
+			name: 'GUIDR',
+			imageUrl: 'https://morenowtech.com/wp-content/uploads/2019/05/guidr.jpg'
+		},
+		{
+			name: 'Random Project',
+			imageUrl:
+				'https://morenowtech.com/wp-content/uploads/2015/01/soy-el-guia.jpg'
+		},
+		{
+			name: 'One More Random Project',
+			imageUrl: 'https://morenowtech.com/wp-content/uploads/2015/01/Marvic.jpg'
+		},
+		{
+			name: 'Blog project',
+			imageUrl:
+				'https://morenowtech.com/wp-content/uploads/2015/01/thedroptv.jpg'
+		},
+		{
+			name: 'Occupational Therapy',
+			imageUrl:
+				'https://morenowtech.com/wp-content/uploads/2015/01/TheAptusGroup.jpg'
+		}
+	];
 
-      imageUrl:
-        "https://www.uncommongoods.com/images/items/46700/46720_1_360px.jpg"
-    },
-    {
-      name: "Shakespearean Insults Chart",
+	const params = {
+		effect: 'coverflow',
+		grabCursor: true,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		coverflowEffect: {
+			rotate: 50,
+			stretch: 0,
+			depth: 100,
+			modifier: 1,
+			slideShadows: true
+		},
+		pagination: {
+			el: '.swiper-pagination'
+		},
+		loop: true
+	};
 
-      imageUrl:
-        "https://www.uncommongoods.com/images/items/27300/27303_1_360px.jpg"
-    },
-    {
-      name: "Yoga Joes",
+	const dataEx = projects_shocase;
 
-      imageUrl:
-        "https://www.uncommongoods.com/images/items/43200/43212_1_360px.jpg"
-    },
-    {
-      name: "Dinosaur Taco Holders",
-
-      imageUrl:
-        "https://www.uncommongoods.com/images/items/45700/45759_1_360px.jpg"
-    },
-    {
-      name: "Color Changing Cinema Lightbox",
-
-      imageUrl:
-        "https://www.uncommongoods.com/images/items/46600/46667_1_360px.jpg"
-    },
-    {
-      name: "Avocado Tree Starter Kit - Set of 3",
-
-      imageUrl:
-        "https://www.uncommongoods.com/images/items/40800/40804_1_360px.jpg"
-    },
-    {
-      name: "Hero Bookend",
-
-      imageUrl:
-        "https://www.uncommongoods.com/images/items/47500/47500_1_360px.jpg"
-    },
-    {
-      name: "Fishing Pole Campfire Roaster",
-
-      imageUrl:
-        "https://www.uncommongoods.com/images/items/43000/43021_1_360px.jpg"
-    },
-    {
-      name: "Elwood the Unicorn Cereal Bowl",
-      imageUrl:
-        "https://www.uncommongoods.com/images/items/25800/25893_1_360px.jpg"
-    },
-    {
-      name: "Spotted Wellies Garden Ducks",
-
-      imageUrl:
-        "https://www.uncommongoods.com/images/items/26500/26530_1_360px.jpg"
-    },
-    {
-      name: "Eye Glasses Holder",
-      id: 11,
-      price: 15.49,
-      imageUrl:
-        "https://www.uncommongoods.com/images/items/19000/19079_1_360px.jpg"
-    }
-  ];
-
-  const params = {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true
-    },
-    pagination: {
-      el: ".swiper-pagination"
-    },
-    loop: true
-  };
-
-  const dataEx = projects_shocase;
-
-  return (
-    <div>
-      <Swiper {...params}>
-        {dataEx.map((x, index) => (
-          <div className="cover-img" key={index}>
-            <a
-              key={x.project_id}
-              className="project-card"
-              href={`/student/project-view/${x.project_id}`}
-            >
-              <img src={x.imageUrl} alt="Project media" />
-              <h3>{x.name}</h3>
-              <button>Learn More</button>
-            </a>
-          </div>
-        ))}
-      </Swiper>
-    </div>
-  );
+	return (
+		<div>
+			<Swiper {...params}>
+				{dataEx.map((x, index) => (
+					<div className="cover-img" key={index}>
+						<a
+							key={x.project_id}
+							className="project-card"
+							href={`/student/project-view/${x.project_id}`}
+						>
+							<img src={x.imageUrl} alt="Project media" />
+							<h3>{x.name}</h3>
+							<button>Learn More</button>
+						</a>
+					</div>
+				))}
+			</Swiper>
+		</div>
+	);
 };
 
 export default ProjectViewHome;
--- src/components/student/profile/StudentProfile.js
+++ src/components/student/profile/StudentProfile.js
@@ -1,122 +1,127 @@
-import React from "react";
-import { Link, withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import { getData } from "./studentProfileActions";
-import badge from "../../../assets/lambda-badge.png";
-import tony from "../../../assets/tony.jpg";
-import Projects from "../projectcards/ProjectCards";
-import NotEndorsed from "../notEndorsed/NotEndorsed";
-import AboutMe from "../aboutMe/AboutMe";
-import Endorsements from "../endorsements/Endorsements";
-import Share from "../share/Share";
-import Status from "../status/Status";
-import Skills from "../skills/Skills";
-import Progress from "../progress/Progress";
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getData } from './studentProfileActions';
+import badge from '../../../assets/lambda-badge.png';
+import tony from '../../../assets/tony.jpg';
+import Projects from '../projectcards/ProjectCards';
+import NotEndorsed from '../notEndorsed/NotEndorsed';
+import AboutMe from '../aboutMe/AboutMe';
+import Endorsements from '../endorsements/Endorsements';
+import Share from '../share/Share';
+import Status from '../status/Status';
+import Skills from '../skills/Skills';
+import Progress from '../progress/Progress';
 
 // yarn add react-id-swiper@latest swiper@latest
 
 class StudentProfile extends React.Component {
-  componentDidMount() {
-    this.props.getData(this.props.match.params.id);
-  }
+	componentDidMount() {
+		this.props.getData(this.props.match.params.id);
+	}
 
-  render() {
-    const {
-      id,
-      name,
-      about,
-      desired_locations,
-      endorsed,
-      endorsements,
-      skills,
-      top_skills,
-      location,
-      linkedin,
-      twitter,
-      github,
-      website,
-      acclaim,
-      cohort,
-      desired_position,
-      projects,
-      top_projects
-    } = this.props.studentProfile.profile;
+	render() {
+		const {
+			id,
+			name,
+			about,
+			desired_locations,
+			endorsed,
+			endorsements,
+			skills,
+			top_skills,
+			location,
+			linkedin,
+			twitter,
+			github,
+			website,
+			acclaim,
+			cohort,
+			desired_position,
+			projects,
+			top_projects,
+			track,
+			profile_pic
+		} = this.props.studentProfile.profile;
 
-    const sameUser = (id === this.props.loggedInProfile.id);
+		const sameUser = id === this.props.loggedInProfile.id;
 
-    return (
-      <div className="student-dashboard">
-        {sameUser && !endorsed && <NotEndorsed />}
-        <header>
-          <div className="profile-container">
-            <div className="picture">
-              <img src={tony} alt="Tony Stark" />
-            </div>
-            <div className="name-cont">
-              <h1>{name}</h1>
-              <h2>{desired_position}</h2>
-              <h3>{location}</h3>
-            </div>
-            <div className="badge">
-              <a rel="noopener noreferrer" href={acclaim} target="_blank">
-                <img src={badge} alt="Lambda Badge" />
-              </a>
-              <div className="contact-btn">
-                <Link to="/contact-me">Contact Me</Link>
-                <Share studentId={id} name={name} />
-              </div>
-            </div>
-            <div className="social-links">
-              <a
-                className="portfolio-btn"
-                rel="noopener noreferrer"
-                href={website}
-                target="_blank"
-              >
-                Portfolio
-              </a>
-              <a rel="noopener noreferrer" href={linkedin} target="_blank">
-                <i className="fab fa-linkedin-in" />
-              </a>
-              <a rel="noopener noreferrer" href={github} target="_blank">
-                <i className="fab fa-github" />
-              </a>
-              <a rel="noopener noreferrer" href={twitter} target="_blank">
-                <i className="fab fa-twitter" />
-              </a>
-            </div>
-          </div>
-        </header>
-        <main>
-          <AboutMe about={about} />
-          <Endorsements endorsements={endorsements || []} />
-          { sameUser && <Progress /> }
-          <hr />
-          <div className="status-skills">
-            <Status
-              cohort={cohort}
-              desired_locations={desired_locations || []}
-              desired_position={desired_position}
-            />
-            <Skills skills={skills || []} top_skills={top_skills || []} />
-          </div>
-          <hr />
-          <Projects projects={projects || []} top_projects={top_projects || []} />
-        </main>
-      </div>
-    );
-  }
+		return (
+			<div className="student-dashboard">
+				{sameUser && !endorsed && <NotEndorsed />}
+				<header>
+					<div className="profile-container">
+						<div className="picture">
+							<img src={profile_pic} alt="Profile picture" />
+						</div>
+						<div className="name-cont">
+							<h1>{name}</h1>
+							<h2>{track}</h2>
+							<h3>{location}</h3>
+						</div>
+						<div className="badge">
+							<a rel="noopener noreferrer" href={acclaim} target="_blank">
+								<img src={badge} alt="Lambda Badge" />
+							</a>
+							<div className="contact-btn">
+								<Link to="/contact-me">Contact Me</Link>
+								<Share studentId={id} name={name} />
+							</div>
+						</div>
+						<div className="social-links">
+							<a
+								className="portfolio-btn"
+								rel="noopener noreferrer"
+								href={website}
+								target="_blank"
+							>
+								Portfolio
+							</a>
+							<a rel="noopener noreferrer" href={linkedin} target="_blank">
+								<i className="fab fa-linkedin-in" />
+							</a>
+							<a rel="noopener noreferrer" href={github} target="_blank">
+								<i className="fab fa-github" />
+							</a>
+							<a rel="noopener noreferrer" href={twitter} target="_blank">
+								<i className="fab fa-twitter" />
+							</a>
+						</div>
+					</div>
+				</header>
+				<main>
+					<AboutMe about={about} />
+					<Endorsements endorsements={endorsements || []} />
+					{sameUser && <Progress />}
+					<hr />
+					<div className="status-skills">
+						<Status
+							cohort={cohort}
+							desired_locations={desired_locations || []}
+							desired_position={desired_position}
+						/>
+						<Skills skills={skills || []} top_skills={top_skills || []} />
+					</div>
+					<hr />
+					<Projects
+						projects={projects || []}
+						top_projects={top_projects || []}
+					/>
+				</main>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => ({
-  ...state,
-  loggedInProfile: state.profile.profileData,
-  studentProfile: state.studentProfile
+	...state,
+	loggedInProfile: state.profile.profileData,
+	studentProfile: state.studentProfile
 });
 
 export default withRouter(
-  connect(
-    mapStateToProps,
-    { getData }
-  )(StudentProfile)
+	connect(
+		mapStateToProps,
+		{ getData }
+	)(StudentProfile)
 );
