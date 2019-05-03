27a9567527d12f52ee0b8bc406a39f5c58f41f9d
Brandon G-Home <brandon.gardner2@outlook.com>
2019-05-03 16:43:20 -0500

    Adds a fix for reveal project animation

--- src/components/student/projectcards/ProjectCards.js
+++ src/components/student/projectcards/ProjectCards.js
@@ -1,110 +1,112 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Fade from 'react-reveal/Fade';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Fade from "react-reveal/Fade";
 
 const ProjectCards = ({ top_projects, projects }) => {
-	const [hide, toggleHide] = useState(true);
+  const [hide, toggleHide] = useState(true);
 
-	top_projects = [
-		{
-			name: 'Brandons project',
-			project_id: 1,
-			type: 'React Application with cool stuff',
-			media: [
-				'https://morenowtech.com/wp-content/uploads/2019/05/tabless-thursday.jpg'
-			]
-		},
-		{
-			name: 'Brandons project',
-			project_id: 2,
-			type: 'React Application with cool stuff',
-			media: ['https://morenowtech.com/wp-content/uploads/2019/05/fit-me.jpg']
-		},
-		{
-			name: 'Brandons project',
-			project_id: 3,
-			type: 'React Application with cool stuff',
-			media: ['https://morenowtech.com/wp-content/uploads/2019/05/tico.jpg']
-		}
-	];
-	projects = [
-		{
-			name: 'Brandons project',
-			project_id: 4,
-			type: 'React Application with cool stuff',
-			media: ['https://morenowtech.com/wp-content/uploads/2019/05/guidr.jpg']
-		},
-		{
-			name: 'Brandons project',
-			project_id: 5,
-			type: 'React Application with cool stuff',
-			media: [
-				'https://morenowtech.com/wp-content/uploads/2015/01/soy-el-guia.jpg'
-			]
-		},
-		{
-			name: 'Brandons project',
-			project_id: 6,
-			type: 'React Application with cool stuff',
-			media: ['https://morenowtech.com/wp-content/uploads/2015/01/Marvic.jpg']
-		},
-		{
-			name: 'Brandons project',
-			project_id: 7,
-			type: 'React Application with cool stuff',
-			media: [
-				'https://morenowtech.com/wp-content/uploads/2015/01/thedroptv.jpg'
-			]
-		},
-		{
-			name: 'Brandons project',
-			project_id: 8,
-			type: 'React Application with cool stuff',
-			media: [
-				'https://morenowtech.com/wp-content/uploads/2015/01/TheAptusGroup.jpg'
-			]
-		},
-		{
-			name: 'Brandons project',
-			project_id: 9,
-			type: 'React Application with cool stuff',
-			media: [
-				'https://assets.entrepreneur.com/content/3x2/2000/how-read-website-source-code.jpg?width=700&crop=2:1'
-			]
-		}
-	];
+  top_projects = [
+    {
+      name: "Brandons project",
+      project_id: 1,
+      type: "React Application with cool stuff",
+      media: [
+        "https://morenowtech.com/wp-content/uploads/2019/05/tabless-thursday.jpg"
+      ]
+    },
+    {
+      name: "Brandons project",
+      project_id: 2,
+      type: "React Application with cool stuff",
+      media: ["https://morenowtech.com/wp-content/uploads/2019/05/fit-me.jpg"]
+    },
+    {
+      name: "Brandons project",
+      project_id: 3,
+      type: "React Application with cool stuff",
+      media: ["https://morenowtech.com/wp-content/uploads/2019/05/tico.jpg"]
+    }
+  ];
+  projects = [
+    {
+      name: "Brandons project",
+      project_id: 4,
+      type: "React Application with cool stuff",
+      media: ["https://morenowtech.com/wp-content/uploads/2019/05/guidr.jpg"]
+    },
+    {
+      name: "Brandons project",
+      project_id: 5,
+      type: "React Application with cool stuff",
+      media: [
+        "https://morenowtech.com/wp-content/uploads/2015/01/soy-el-guia.jpg"
+      ]
+    },
+    {
+      name: "Brandons project",
+      project_id: 6,
+      type: "React Application with cool stuff",
+      media: ["https://morenowtech.com/wp-content/uploads/2015/01/Marvic.jpg"]
+    },
+    {
+      name: "Brandons project",
+      project_id: 7,
+      type: "React Application with cool stuff",
+      media: [
+        "https://morenowtech.com/wp-content/uploads/2015/01/thedroptv.jpg"
+      ]
+    },
+    {
+      name: "Brandons project",
+      project_id: 8,
+      type: "React Application with cool stuff",
+      media: [
+        "https://morenowtech.com/wp-content/uploads/2015/01/TheAptusGroup.jpg"
+      ]
+    },
+    {
+      name: "Brandons project",
+      project_id: 9,
+      type: "React Application with cool stuff",
+      media: [
+        "https://morenowtech.com/wp-content/uploads/2015/01/TheAptusGroup.jpg"
+      ]
+    }
+  ];
 
-	const map = arr =>
-		arr.map(proj => (
-			<Link
-				key={proj.project_id}
-				className="project-card"
-				to={`/student/project-view/${proj.project_id}`}
-			>
-				<img src={proj.media[0]} alt="Project media" />
-				<h3>{proj.name}</h3>
-				<p>{proj.type}</p>
-			</Link>
-		));
-	return (
-		<div className="projects-wrapper">
-			<h2>
-				<i className="fas fa-project-diagram" /> Projects
-			</h2>
-			<div className="projects-inner-wrapper">
-				{map(top_projects)}
-				<Fade collapse when={!hide}>
-					<div>{map(projects)}</div>
-				</Fade>
-			</div>
-			<div className="projects-buttons-container">
-				<button type="button" onClick={() => toggleHide(!hide)}>
-					{hide ? 'Show More' : 'Show Less '}
-					<i className="fas fa-chevron-down" />
-				</button>
-			</div>
-		</div>
-	);
+  const map = arr =>
+    arr.map(proj => (
+      <Link
+        key={proj.project_id}
+        className="project-card"
+        to={`/student/project-view/${proj.project_id}`}
+      >
+        <img src={proj.media[0]} alt="Project media" />
+        <h3>{proj.name}</h3>
+        <p>{proj.type}</p>
+      </Link>
+    ));
+  return (
+    <div className="projects-wrapper">
+      <h2>
+        <i className="fas fa-project-diagram" /> Projects
+      </h2>
+      <div className="projects-inner-wrapper">
+        {map(top_projects)}
+        <Fade collapse when={!hide}>
+          <div style={hide ? { display: "none" } : { display: "flex" }}>
+            {map(projects)}
+          </div>
+        </Fade>
+      </div>
+      <div className="projects-buttons-container">
+        <button type="button" onClick={() => toggleHide(!hide)}>
+          {hide ? "Show More" : "Show Less "}
+          <i className={hide ? "fas fa-chevron-down" : "fas fa-chevron-up"} />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectCards;
