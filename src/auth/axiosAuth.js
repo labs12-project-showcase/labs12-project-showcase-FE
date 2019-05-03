3134b3389f9fe367aaa237de26a258182e17e0d1
Brandon G-Home <brandon.gardner2@outlook.com>
2019-05-03 12:06:32 -0500

    Pushes current working code up

--- src/auth/axiosAuth.js
+++ src/auth/axiosAuth.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 /**
  * Retrieves the JWT from `localStorage` and adds it
- * to the `headers` in the `axios` call.  
+ * to the `headers` in the `axios` call.
  * Example use: `import axiosAuth from 'path'; axiosAuth().put(...);`
  */
 export default function() {
-    const token = localStorage.getItem('backendToken');
+  const token = localStorage.getItem("backendToken");
 
-    return axios.create({
-        headers: {
-            // 'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        }
-    });
-};
+  return axios.create({
+    headers: {
+      // 'Content-Type': 'application/json',
+      authorization: token
+    }
+  });
+}
--- src/components/student/projectcards/EditCards.js
+++ src/components/student/projectcards/EditCards.js
@@ -1,26 +1,7 @@
 import React, { useState } from "react";
 
-const EditCards = ({ id, top_projects, projects }) => {
-  //New Top Projects should initialize with the list of top projects from props
-  const [newTopProjs, updateTopProjs] = useState(top_projects || []);
-  //New Projects should initialize with the list of projects from props
-  const [newProjs, updateProjs] = useState(projects || []);
+const EditCards = ({ top_projects, projects, setProjects, setTopProjects }) => {
   const [dragged, updateDragged] = useState({});
-
-  const handleSubmit = () => {
-    const top_projects = newTopProjs.map(proj => ({
-      project_id: proj.project_id,
-      student_id: id
-    }));
-    const projects = newProjs.map(proj => ({
-      project_id: proj.project_id,
-      student_id: id
-    }));
-
-    //#TODO Submit Action here for projects page
-  };
-
-  //#TODO Cancel changes action here maybe??
 
   const beginDrag = (e, index) => {
     e.preventDefault();
@@ -32,18 +13,18 @@ const EditCards = ({ id, top_projects, projects }) => {
     const topIndex = Number(e.target.dataset.index);
 
     //Find the correct projects from each array
-    const newTop = newProjs[dragged];
-    const newBottom = newTopProjs[topIndex];
+    const newTop = projects[dragged];
+    const newBottom = top_projects[topIndex];
 
     //Reduce each array with new projects in place of old ones
-    const newTops = newTopProjs.reduce((arr, cur, index) => {
+    const newTops = top_projects.reduce((arr, cur, index) => {
       if (index === topIndex) {
         return [...arr, newTop];
       }
       return [...arr, cur];
     }, []);
 
-    const newBottoms = newProjs.reduce((arr, cur, index) => {
+    const newBottoms = projects.reduce((arr, cur, index) => {
       if (index === dragged) {
         return [...arr, newBottom];
       }
@@ -51,8 +32,10 @@ const EditCards = ({ id, top_projects, projects }) => {
     }, []);
 
     //Update state of top projects and projects. Also clear dragged index.
-    updateTopProjs(newTops);
-    updateProjs(newBottoms);
+    console.log("NEW TOP PROJECTS", newTops);
+    console.log("NEW PROJECTS", newBottoms);
+    setTopProjects(newTops);
+    setProjects(newBottoms);
     updateDragged({});
   };
 
@@ -63,7 +46,7 @@ const EditCards = ({ id, top_projects, projects }) => {
   //Dragging should only be available from lower project => upper project area
   const draggableMap = arr =>
     arr.map((proj, index) => (
-      <div key={proj.project_id} className="project-card drag-drop-target">
+      <div key={proj.id} className="project-card drag-drop-target">
         <div
           className="drag-target-cover"
           draggable
@@ -78,7 +61,7 @@ const EditCards = ({ id, top_projects, projects }) => {
   const targetMap = arr =>
     arr.map((proj, index) => (
       <div
-        key={proj.project_id}
+        key={proj.id}
         onDrop={e => handleDrop(e)}
         onDragOver={e => handleDragOver(e)}
         className="project-card drag-drop-target"
@@ -94,24 +77,11 @@ const EditCards = ({ id, top_projects, projects }) => {
   return (
     <div className="projects-wrapper">
       <div className="projects-inner-wrapper">
-        {targetMap(newTopProjs)}
+        {targetMap(top_projects)}
         <p className="edit-project-message">
           Please drag projects from below to the top projects section.
         </p>
-        {draggableMap(newProjs)}
-      </div>
-      <div className="projects-buttons-container">
-        <button type="button" onClick={handleSubmit}>
-          Submit Projects
-        </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Need cancel changes handler");
-          }}
-        >
-          Cancel
-        </button>
+        {draggableMap(projects)}
       </div>
     </div>
   );
