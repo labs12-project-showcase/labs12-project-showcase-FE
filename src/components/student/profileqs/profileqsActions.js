b4abb7914fc8bdc61b14f154f29558b5853b119c
Brandon G-Home <brandon.gardner2@outlook.com>
2019-05-03 17:48:04 -0500

    Includes auth issue fix for edit profile

--- src/components/student/profileqs/profileqsActions.js
+++ src/components/student/profileqs/profileqsActions.js
@@ -1,11 +1,8 @@
 import history from "../../../history.js";
-import axios from "axios";
-
-const token = localStorage.getItem("backendToken");
+import axiosAuth from "../../../auth/axiosAuth";
 
 const backendURL = "https://halg-backend.herokuapp.com";
 
-//
 export const GET_PROFILE_DATA_FAILURE = "GET_PROFILE_DATA_FAILURE";
 export const GET_PROFILE_DATA_START = "GET_PROFILE_DATA_START";
 export const GET_PROFILE_DATA_SUCCESS = "GET_PROFILE_DATA_SUCCESS";
@@ -19,12 +16,8 @@ export const getProfileData = (queryUpdate = false) => dispatch => {
   let url = `${backendURL}/api/students/profile${
     queryUpdate ? "?update=true" : ""
   }`;
-  axios
-    .get(url, {
-      headers: {
-        authorization: token
-      }
-    })
+  axiosAuth()
+    .get(url)
     .then(res => {
       dispatch({
         type: GET_PROFILE_DATA_SUCCESS,
@@ -75,15 +68,8 @@ export const updateProfile = formValues => dispatch => {
   };
   console.log("send", send);
   dispatch({ type: UPDATE_PROFILE_START });
-  if (!token)
-    return dispatch({
-      type: UPDATE_PROFILE_FAILURE,
-      payload: "No token found in Local Storage"
-    });
-  axios
-    .put(`${backendURL}/api/students/update`, removeEmptyValues(send), {
-      headers: { authorization: token }
-    })
+  axiosAuth()
+    .put(`${backendURL}/api/students/update`, removeEmptyValues(send))
     .then(res => {
       history.push(`/student/profile/${formValues.id}`);
       dispatch({
