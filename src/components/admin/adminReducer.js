46ed3a30be3b6c8fbf3db9c892d920605669f0fe
Jake Thomas <jmaxt12@gmail.com>
2019-05-07 14:09:13 -0600

    Added actions for the project view

--- src/components/admin/adminReducer.js
+++ src/components/admin/adminReducer.js
@@ -1,9 +1,11 @@
 import {
-  ADMIN_FETCHED_STUDENTS
+  ADMIN_FETCHED_STUDENTS,
+  ADMIN_FETCHED_PROJECTS
 } from "./adminActions";
 
 const initialState = {
-  students: []
+  students: [],
+  projects: []
 };
 
 const adminReducer = (state = initialState, action) => {
@@ -12,6 +14,11 @@ const adminReducer = (state = initialState, action) => {
       return {
         ...state,
         students: action.payload
+      }
+      case ADMIN_FETCHED_PROJECTS:
+      return {
+        ...state,
+        projects: action.payload
       }
     default:
       return state;
