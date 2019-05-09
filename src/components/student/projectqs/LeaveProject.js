da63403c2be70b47bcc6fc8d622cc105c3a9335b
Brandon G-Home <brandon.gardner2@outlook.com>
2019-05-09 11:41:08 -0500

    Adds last member logic for leave project

--- src/components/student/projectqs/LeaveProject.js
+++ src/components/student/projectqs/LeaveProject.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { withRouter, Link } from "react-router-dom";
 import { connect } from "react-redux";
 
@@ -8,30 +8,91 @@ import { getProject } from "./projectqsActions";
 const LeaveProject = ({
   dispatch,
   students,
+  history,
+  student_id,
   match: {
-    params: { id }
+    params: { id: project_id }
   }
 }) => {
+  const [modal, setModal] = useState(false);
+
   const leave = () => {
-    axiosAuth
-      .put(`https://halg-backend.herokuapp.com/api/projects/leave/${id}`)
+    if (checkMembers) {
+      openModal();
+    } else {
+      dispatchLeave()
+        .then(() => {
+          dispatch(getProject(project_id));
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+
+  const cancelLeave = () => {
+    closeModal();
+  };
+
+  const confirmLeave = () => {
+    dispatchLeave()
       .then(() => {
-        dispatch(getProject(id));
+        history.push(`/student/profile/${student_id}`);
       })
       .catch(err => {
         console.log(err);
       });
   };
+
+  const dispatchLeave = () => {
+    return axiosAuth.put(
+      `https://halg-backend.herokuapp.com/api/projects/leave/${project_id}`
+    );
+  };
+
+  const checkMembers = () => {
+    if (students.length === 1) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const openModal = () => {
+    setModal(true);
+  };
+
+  const closeModal = () => {
+    setModal(false);
+  };
   return (
-    <Link to="#" onClick={leave}>
-      <i className="fas fa-arrow-left" />
-      Leave Project
-    </Link>
+    <React.Fragment>
+      <Link to="#" onClick={leave}>
+        <i className="fas fa-arrow-left" />
+        Leave Project
+      </Link>
+      {modal ? (
+        <div className="modal">
+          <h2>Alert</h2>
+          <p>
+            You are the last member on this project. If you leave, the project
+            will be deleted. This cannot be reversed. Are you sure?
+          </p>
+          <button type="button" className="modal-button" onClick={confirmLeave}>
+            I'm Sure
+          </button>
+          <button type="button" className="modal-button" onClick={cancelLeave}>
+            Cancel
+          </button>
+        </div>
+      ) : null}
+    </React.Fragment>
   );
 };
 
 const mapStateToProps = state => ({
-  students: state.project.projectData.students
+  students: state.project.projectData.students,
+  student_id: state.profile.profileData.id
 });
 
 export default withRouter(connect(mapStateToProps)(LeaveProject));
