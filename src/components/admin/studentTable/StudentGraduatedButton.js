e0e748897fe0fbdb6c4ee6e34cd13241808991ce
Jake Thomas <jmaxt12@gmail.com>
2019-05-10 14:21:42 -0600

    Added components for student switches

---
+++ src/components/admin/studentTable/StudentGraduatedButton.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { connect } from 'react-redux';
+import Switch from "@material-ui/core/Switch";
+import { updateProject } from '../adminActions';
+
+class StudentGraduatedButton extends React.Component {
+    handleChange = (e) => {
+        e.stopPropagation();
+        this.props.updateProject(this.props.project.id, {
+            approved: !this.props.project.approved
+        });
+    }
+
+    render() { 
+        console.log(this.props.project);
+        return (  
+            <Switch
+                onClick={this.handleChange}
+                checked={this.props.project.approved}
+                color="primary"
+          />
+        );
+    }
+}
+ 
+export default connect(null, { updateProject })(StudentGraduatedButton);
+
