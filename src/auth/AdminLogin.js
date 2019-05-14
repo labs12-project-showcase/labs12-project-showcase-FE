e834c9fb0ecf2d70a279cde19e09e91536212617
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-05-13 22:50:38 -0400

    Adds some temporary swaggy styles to liven up the 'Admin Login' button to reminisce on my Saturday mornings as a child

--- src/auth/AdminLogin.js
+++ src/auth/AdminLogin.js
@@ -1,12 +1,24 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { adminLogin } from './authActions.js';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { adminLogin } from "./authActions.js";
 
 class AdminLogin extends Component {
   render() {
-    return ( 
+    return (
       <div className="adminLogin">
-        <button onClick={this.props.adminLogin}>Admin Login</button>
+        <p className="adminLogin-text">"You've found me lucky login button!"</p>
+        <iframe
+          src="https://giphy.com/embed/3orieXPC47dgvZKEXm"
+          title="lucky charms"
+          width="240"
+          height="182"
+          frameBorder="0"
+          className="giphy-embed"
+          allowFullScreen
+        />
+        <button className="adminLogin-btn" onClick={this.props.adminLogin}>
+          Admin Login
+        </button>
       </div>
     );
   }
@@ -15,7 +27,10 @@ class AdminLogin extends Component {
 const mapStateToProps = state => {
   return {
     ...state
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, { adminLogin })(AdminLogin);
+export default connect(
+  mapStateToProps,
+  { adminLogin }
+)(AdminLogin);
