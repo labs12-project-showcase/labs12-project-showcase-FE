603d72c0abec29595f12b9249c9b99eed8765225
Brandon G-Home <brandon.gardner2@outlook.com>
2019-05-14 10:49:28 -0500

    removes service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import { Router } from "react-router-dom";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
@@ -41,8 +40,3 @@ ReactDOM.render(
 
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
