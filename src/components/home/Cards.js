f0bedf0a3d0401765666df09efe7257a2e37de19
Brandon G-Home <brandon.gardner2@outlook.com>
2019-05-14 17:36:23 -0500

    Changes cards to show location instead of social links

--- src/components/home/Cards.js
+++ src/components/home/Cards.js
@@ -1,57 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Cards = ({ cards }) => {
-	const map = arr => arr.map(skill => <li key={skill}>{skill}</li>);
-	return (
-		<>
-			<div className="cards">
-				<div className="profile-pic">
-					<Link to={`/student/profile/${cards.id}`}>
-						<img src={cards.profile_pic} alt="Profile" />
-					</Link>
-				</div>
-				<div className="details">
-					<h2>{cards.name}</h2>
-					<h3>{cards.track}</h3>
+  const map = arr => arr.map(skill => <li key={skill}>{skill}</li>);
+  return (
+    <>
+      <div className="cards">
+        <div className="profile-pic">
+          <Link to={`/student/profile/${cards.id}`}>
+            <img src={cards.profile_pic} alt="Profile" />
+          </Link>
+        </div>
+        <div className="details">
+          <h2>{cards.name}</h2>
+          <h3>{cards.track}</h3>
 
-					<br />
+          <br />
 
-					<h4>Skills:</h4>
-					<ul>{map(cards.top_skills)}</ul>
-					<div className="contact-links">
-						<a href={cards.github} target="_blank" rel="noopener noreferrer">
-							<i className="fab fa-github" />
-						</a>
-						<a href={cards.linkedin} target="_blank" rel="noopener noreferrer">
-							<i className="fab fa-linkedin-in" />
-						</a>
-						<a href={cards.twitter} target="_blank" rel="noopener noreferrer">
-							<i className="fab fa-twitter" />
-						</a>
-						<Link to={`/student/profile/${cards.id}`}>
-							<button className="prof-link">Profile &amp; Projects</button>
-						</Link>
-					</div>
-				</div>
-				<div className="projects-box">
-					<h2>My top 3 projects</h2>
-					{cards.top_projects.map(proj => (
-						<Link
-							key={proj.project_id}
-							to={`/student/project-view/${proj.project_id}`}
-							className="display-box"
-						>
-							<div className="proj-box-container">
-								<img src={proj.media} alt="Project" />
-								<h3>{proj.name}</h3>
-							</div>
-						</Link>
-					))}
-				</div>
-			</div>
-		</>
-	);
+          <h4>Skills:</h4>
+          <ul>{map(cards.top_skills)}</ul>
+          <div className="contact-links">
+            <span>
+              <i className="fas fa-map-marker-alt icon-grey" />
+              {cards.location}
+            </span>
+            <Link to={`/student/profile/${cards.id}`}>
+              <button className="prof-link">Profile &amp; Projects</button>
+            </Link>
+          </div>
+        </div>
+        <div className="projects-box">
+          <h2>My top 3 projects</h2>
+          {cards.top_projects.map(proj => (
+            <Link
+              key={proj.project_id}
+              to={`/student/project-view/${proj.project_id}`}
+              className="display-box"
+            >
+              <div className="proj-box-container">
+                <img src={proj.media} alt="Project" />
+                <h3>{proj.name}</h3>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Cards;
