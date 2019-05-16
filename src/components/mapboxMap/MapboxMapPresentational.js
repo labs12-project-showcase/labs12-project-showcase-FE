940ff8140f98450d3b9990907f2f1851557a0996
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-05-16 17:24:50 -0400

    Removes unnecessary files from 'mapboxMap' folder

--- src/components/mapboxMap/MapboxMapPresentational.js
+++ src/components/mapboxMap/MapboxMapPresentational.js
@@ -4,13 +4,11 @@ import { render } from "react-dom";
 
 import ReactMapGL, {
   Marker,
-  Popup,
   FullscreenControl,
   NavigationControl
 } from "react-map-gl";
 
 import StudentPin from "./student-pin";
-import StudentInfo from "./student-info";
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
@@ -72,32 +70,13 @@ class MapboxMapPresentational extends React.Component {
     );
   };
 
-  // _renderPopup() {
-  //   const { popupInfo } = this.state;
-
-  //   return (
-  //     popupInfo && (
-  //       <Popup
-  //         tipSize={6}
-  //         anchor="top"
-  //         longitude={popupInfo.longitude}
-  //         latitude={popupInfo.latitude}
-  //         closeOnClick={false}
-  //         onClose={() => this.setState({ popupInfo: null })}
-  //       >
-  //         <StudentInfo info={popupInfo} />
-  //       </Popup>
-  //     )
-  //   );
-  // }
-
   render() {
     const { viewport } = this.state;
     return (
       <ReactMapGL
         {...viewport}
-        mapStyle="mapbox://styles/mapbox/dark-v10"
-        // mapStyle="mapbox://styles/mapbox/streets-v11"
+        // mapStyle="mapbox://styles/mapbox/dark-v10"
+        mapStyle="mapbox://styles/mapbox/streets-v11"
         onViewportChange={this._updateViewport}
         mapboxApiAccessToken={MAPBOX_TOKEN}
         scrollZoom={false}
