8bd2fdfe8d3c2c03ad23b2ca82280e162e7f7215
Brandon G-Home <brandon.gardner2@outlook.com>
2019-05-16 12:22:28 -0500

    Moves drag and drop script and fixes another class

--- src/components/student/profileqs/Profileqs.js
+++ src/components/student/profileqs/Profileqs.js
@@ -1,92 +1,98 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import DeleteModal from '../profile/DeleteModal.js';
-import EditImage from '../../EditImage/EditImage';
+import React from "react";
+import { connect } from "react-redux";
+import DeleteModal from "../profile/DeleteModal.js";
+import EditImage from "../../EditImage/EditImage";
 import {
-	deleteProfilePicture,
-	getProfileData,
-	updateProfile,
-	uploadProfilePicture
-} from './profileqsActions';
-import avatar from '../../../assets/avatar.jpg';
-import ProfileqsForm from './ProfileqsForm';
+  deleteProfilePicture,
+  getProfileData,
+  updateProfile,
+  uploadProfilePicture
+} from "./profileqsActions";
+import avatar from "../../../assets/avatar.jpg";
+import ProfileqsForm from "./ProfileqsForm";
+
+import DragDropScript from "../../../config/mobiledragdrop";
 
 class Profileqs extends React.Component {
-	userExists = this.props.profile.profileData.exists || false;
+  userExists = this.props.profile.profileData.exists || false;
 
-	componentDidMount() {
-		if (
-			// check for `name`, which is a required field and
-			// therefore indicates if we have the User info
-			!this.props.profile.profileData.name ||
-			// check for `cohort_options`, which we need to show on this page
-			!this.props.profile.profileData.cohort_options.length
-		) {
-			// the `true` argument pulls `cohort` and `track` info
-			this.props.getProfileData(true);
-		}
-	}
+  componentDidMount() {
+    if (
+      // check for `name`, which is a required field and
+      // therefore indicates if we have the User info
+      !this.props.profile.profileData.name ||
+      // check for `cohort_options`, which we need to show on this page
+      !this.props.profile.profileData.cohort_options.length
+    ) {
+      // the `true` argument pulls `cohort` and `track` info
+      this.props.getProfileData(true);
+    }
+    const script = document.createElement("script");
+    script.src = DragDropScript;
 
-	render() {
-		return (
-			<div className="profileqs-container">
-				<div className="profileqs">
-					{this.userExists ? (
-						<>
-							<h3>Edit Profile</h3>
-							<p>Make your changes below.</p>
-						</>
-					) : (
-						<>
-							<h3>Profile Quick Start</h3>
-							<p>Use the fields below to build your profile.</p>
-							{this.props.profile.profileData.github ? (
-								<>
-									<p>
-										We have prepopulated some of the fields with information
-										from GitHub. That information will only be saved after you
-										submit the form.
-									</p>
-								</>
-							) : null}
-						</>
-					)}
-					<div className="profile-picture">
-						<DeleteModal />
-						<span className="input-label">Profile Picture</span>
-						<EditImage
-							initialImageList={[this.props.profile.profileData.profile_pic]}
-							maxFileCount={1}
-							onImageUpload={this.props.uploadProfilePicture}
-							onRemove={url => this.props.deleteProfilePicture(url)}
-							onUrlAdd={url =>
-								this.props.updateProfile({ profile_pic: url }, false)
-							}
-							placeholder={avatar}
-							suggestedUrl={
-								this.props.profile.profileData.github
-									? `${this.props.profile.profileData.github}.png`
-									: ''
-							}
-							uploadButtonText="Upload file"
-						/>
-					</div>
-					<ProfileqsForm initialFormValues={this.props.profile.profileData} />
-				</div>
-			</div>
-		);
-	}
+    document.body.appendChild(script);
+  }
+
+  render() {
+    return (
+      <div className="profileqs-container">
+        <div className="profileqs">
+          {this.userExists ? (
+            <>
+              <h3>Edit Profile</h3>
+              <p>Make your changes below.</p>
+            </>
+          ) : (
+            <>
+              <h3>Profile Quick Start</h3>
+              <p>Use the fields below to build your profile.</p>
+              {this.props.profile.profileData.github ? (
+                <>
+                  <p>
+                    We have prepopulated some of the fields with information
+                    from GitHub. That information will only be saved after you
+                    submit the form.
+                  </p>
+                </>
+              ) : null}
+            </>
+          )}
+          <div className="profile-picture">
+            <DeleteModal />
+            <span className="input-label">Profile Picture</span>
+            <EditImage
+              initialImageList={[this.props.profile.profileData.profile_pic]}
+              maxFileCount={1}
+              onImageUpload={this.props.uploadProfilePicture}
+              onRemove={url => this.props.deleteProfilePicture(url)}
+              onUrlAdd={url =>
+                this.props.updateProfile({ profile_pic: url }, false)
+              }
+              placeholder={avatar}
+              suggestedUrl={
+                this.props.profile.profileData.github
+                  ? `${this.props.profile.profileData.github}.png`
+                  : ""
+              }
+              uploadButtonText="Upload file"
+            />
+          </div>
+          <ProfileqsForm initialFormValues={this.props.profile.profileData} />
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-	// console.log('map state to props', state);
-	return {
-		...state.profileqs,
-		profile: state.profile
-	};
+  // console.log('map state to props', state);
+  return {
+    ...state.profileqs,
+    profile: state.profile
+  };
 };
 
 export default connect(
-	mapStateToProps,
-	{ deleteProfilePicture, getProfileData, updateProfile, uploadProfilePicture }
+  mapStateToProps,
+  { deleteProfilePicture, getProfileData, updateProfile, uploadProfilePicture }
 )(Profileqs);
--- src/config/mobiledragdrop.js
+++ src/config/mobiledragdrop.js
@@ -1,6 +1,5 @@
 var DragDropTouch;
 (function(DragDropTouch_1) {
-  "use strict";
   /**
    * Object used to hold the data that is being dragged during drag and drop operations.
    *
@@ -134,7 +133,7 @@ var DragDropTouch;
       this._lastClick = 0;
       // enforce singleton pattern
       if (DragDropTouch._instance) {
-        throw "DragDropTouch instance already created.";
+        throw Error("DragDropTouch instance already created.");
       }
       // listen to touch events
       if ("ontouchstart" in document) {
@@ -183,7 +182,7 @@ var DragDropTouch;
             e.preventDefault();
             // show context menu if the user hasn't started dragging after a while
             setTimeout(function() {
-              if (_this._dragSource == src && _this._img == null) {
+              if (_this._dragSource === src && _this._img == null) {
                 if (_this._dispatchEvent(e, "contextmenu", src)) {
                   _this._reset();
                 }
@@ -215,7 +214,7 @@ var DragDropTouch;
         if (this._img) {
           this._lastTouch = e;
           e.preventDefault(); // prevent scrolling
-          if (target != this._lastTarget) {
+          if (target !== this._lastTarget) {
             this._dispatchEvent(this._lastTouch, "dragleave", this._lastTarget);
             this._dispatchEvent(e, "dragenter", target);
             this._lastTarget = target;
@@ -279,7 +278,7 @@ var DragDropTouch;
     DragDropTouch.prototype._getTarget = function(e) {
       var pt = this._getPoint(e),
         el = document.elementFromPoint(pt.x, pt.y);
-      while (el && getComputedStyle(el).pointerEvents == "none") {
+      while (el && getComputedStyle(el).pointerEvents === "none") {
         el = el.parentElement;
       }
       return el;
@@ -355,7 +354,7 @@ var DragDropTouch;
       }
       dst.style.pointerEvents = "none";
       // and repeat for all children
-      for (var i = 0; i < src.children.length; i++) {
+      for (let i = 0; i < src.children.length; i++) {
         this._copyStyle(src.children[i], dst.children[i]);
       }
     };
