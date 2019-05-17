7ee528a556ec0d5110ef81bbda62a2f73f47f4f6
Julian Moreno <julmo611@gmail.com>
2019-05-17 14:54:40 -0700

    Old branch

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,32 +7,46 @@ export default function Footer() {
 		<>
 			<footer>
 				<nav>
-					<a href="https://lambdaschool.com/courses/cs/web/">COURSES</a>
-					<a href="https://lambdaschool.com/about/">ABOUT</a>
-					<a href="https://lambdaschool.com/blog/">BLOG</a>
-					<a href="https://lambdaschool.com/apply/">APPLY</a>
-					<a href="https://lambdaschool.com/careers/">CAREERS</a>
-					<a href="https://lambdaschool.com/privacy/">LEGAL</a>
-					<a href="https://lambdaschool.com/contact/">CONTACT</a>
-					<a href="https://lambdaschool.com/courses/cs/web/bootcamp/">
-						FREE CODE BOOTCAMP
+					<a href="https://lambdaschool.com/courses/cs/web/">
+						COURSES
 					</a>
-					<a href="https://lambdaschool.com/referral/">REFERRAL</a>
-					<div>
-						<a href="https://twitter.com/lambdaschool">
-							<i className="fab fa-twitter" />
-						</a>
-						<a href="https://www.facebook.com/LambdaSchoolOnline/">
-							<i className="fab fa-facebook" />
-						</a>
-						<a href="https://github.com/LambdaSchool">
-							<i className="fab fa-github" />
-						</a>
-					</div>
+					<a href="https://lambdaschool.com/about/">
+						ABOUT
+					</a>
+					<a href="https://lambdaschool.com/blog/">
+						BLOG
+					</a>
+					<a href="https://lambdaschool.com/apply/">
+						APPLY
+					</a>
+					<a href="https://lambdaschool.com/careers/">
+						CAREERS
+					</a>
+					<a href="https://lambdaschool.com/privacy/">
+						LEGAL
+					</a>
+					<a href="https://lambdaschool.com/contact/">
+						CONTACT
+					</a>
+					<a href="https://lambdaschool.com/courses/cs/web/bootcamp/">
+						FREE CODE BOOTCAMP 
+					</a>
+					<a href="https://lambdaschool.com/referral/">
+						REFERRAL
+					</a>
+					<a href="https://twitter.com/lambdaschool">
+						<i className="fab fa-twitter"></i>
+					</a>
+					<a href="https://www.facebook.com/LambdaSchoolOnline/">
+						<i className="fab fa-facebook"></i>
+					</a>
+					<a href="https://github.com/LambdaSchool">
+						<i className="fab fa-github"></i>
+					</a>
 				</nav>
 				<NavLink exact to="/">
 					<img
-						className="Footer-hire-lambda-logo"
+						className="TopBar-hire-lambda-logo"
 						src={whiteLambdaLogo}
 						alt="white lambda logo"
 					/>
