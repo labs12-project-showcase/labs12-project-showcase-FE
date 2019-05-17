c32dd8ab69b97dc04f0b161d056c61761d06933e
Julian Moreno <julmo611@gmail.com>
2019-05-16 17:40:53 -0700

    About block min-with

--- src/components/mapboxMap/MapboxMapPresentational.js
+++ src/components/mapboxMap/MapboxMapPresentational.js
@@ -1,76 +1,76 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
-import { render } from "react-dom";
-import ReactMapGL, { Marker } from "react-map-gl";
-import StudentPin from "./student-pin";
-import "mapbox-gl/src/css/mapbox-gl.css";
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { render } from 'react-dom';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import StudentPin from './student-pin';
+import 'mapbox-gl/src/css/mapbox-gl.css';
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
 class MapboxMapPresentational extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      viewport: {
-        width: "100vw",
-        height: "100vh",
-        latitude: 39.788260590328576,
-        longitude: -97.77255674948162,
-        zoom: 4,
-        bearing: 0,
-        pitch: 0
-      },
-      popupInfo: null,
-      students: this.props.mapData
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			viewport: {
+				width: '100vw',
+				height: '100vh',
+				latitude: 39.788260590328576,
+				longitude: -97.77255674948162,
+				zoom: 4,
+				bearing: 0,
+				pitch: 0
+			},
+			popupInfo: null,
+			students: this.props.mapData
+		};
+	}
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.mapData !== this.props.mapData) {
-      this.setState({ students: this.props.mapData });
-    }
-  }
+	componentDidUpdate(prevProps) {
+		if (prevProps.mapData !== this.props.mapData) {
+			this.setState({ students: this.props.mapData });
+		}
+	}
 
-  _updateViewport = viewport => {
-    this.setState({ viewport });
-  };
+	_updateViewport = viewport => {
+		this.setState({ viewport });
+	};
 
-  _renderStudentMarker = (student, index) => {
-    return (
-      <Marker
-        key={`marker-${index}`}
-        longitude={parseFloat(student.longitude)}
-        latitude={parseFloat(student.latitude)}
-      >
-        <StudentPin
-          size={20}
-          onClick={() => this.setState({ popupInfo: student })}
-        />
-      </Marker>
-    );
-  };
+	_renderStudentMarker = (student, index) => {
+		return (
+			<Marker
+				key={`marker-${index}`}
+				longitude={parseFloat(student.longitude)}
+				latitude={parseFloat(student.latitude)}
+			>
+				<StudentPin
+					size={20}
+					onClick={() => this.setState({ popupInfo: student })}
+				/>
+			</Marker>
+		);
+	};
 
-  render() {
-    const { viewport } = this.state;
-    return (
-      <ReactMapGL
-        {...viewport}
-        // mapStyle="mapbox://styles/mapbox/dark-v10"
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-        onViewportChange={this._updateViewport}
-        mapboxApiAccessToken={MAPBOX_TOKEN}
-        scrollZoom={false}
-        className="react-map"
-        id="map"
-      >
-        {this.state.students.map(this._renderStudentMarker)}
-      </ReactMapGL>
-    );
-  }
+	render() {
+		const { viewport } = this.state;
+		return (
+			<ReactMapGL
+				{...viewport}
+				// mapStyle="mapbox://styles/mapbox/dark-v10"
+				mapStyle="mapbox://styles/mapbox/streets-v11"
+				onViewportChange={this._updateViewport}
+				mapboxApiAccessToken={MAPBOX_TOKEN}
+				scrollZoom={false}
+				className="react-map"
+				id="map"
+			>
+				{this.state.students.map(this._renderStudentMarker)}
+			</ReactMapGL>
+		);
+	}
 }
 
 export default withRouter(MapboxMapPresentational);
 
 export function renderToDom(container) {
-  render(<MapboxMapPresentational />, container);
+	render(<MapboxMapPresentational />, container);
 }
