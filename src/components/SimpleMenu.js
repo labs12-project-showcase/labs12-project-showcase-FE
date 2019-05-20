5431548f2e4b2a7b7773a3f2b93532ce1e6dde2f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-05-20 00:55:18 -0400

    Creates a <SimpleMenu /> component that will only appear when the device screen width is 500px or less, to increase screen real estate for improved UX on mobile devices

---
+++ src/components/SimpleMenu.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+
+class SimpleMenu extends React.Component {
+  state = {
+    anchorEl: null
+  };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { anchorEl } = this.state;
+
+    return (
+      <div className="simple-menu-container">
+        <Button
+          aria-owns={anchorEl ? "simple-menu" : undefined}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+        >
+          <i
+            className="fas fa-bars"
+            style={{ fontSize: "3rem", color: "white" }}
+          />
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}
+          style={{ zIndex: "100000" }}
+        >
+          <MenuItem onClick={this.handleClose}>Discover</MenuItem>
+          <MenuItem onClick={this.handleClose}>Register/Sign In</MenuItem>
+          <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+        </Menu>
+      </div>
+    );
+  }
+}
+
+export default SimpleMenu;
