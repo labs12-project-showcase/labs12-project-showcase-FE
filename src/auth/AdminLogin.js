d6ecf3f99d7127b88363b5bb4ea85d008aaaf234
Brandon G-Home <brandon.gardner2@outlook.com>
2019-05-21 17:49:10 -0500

    fixes big error from infinite state updates on admin login

--- src/auth/AdminLogin.js
+++ src/auth/AdminLogin.js
@@ -1,24 +1,19 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { adminLogin } from './authActions.js';
-import Loading from '../components/utils/Loading.js';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { adminLogin } from "./authActions.js";
+import Loading from "../components/utils/Loading.js";
 
 class AdminLogin extends Component {
-	render() {
-		this.props.adminLogin();
-		return (
-			<Loading />
-		);
-	}
+  componentDidMount() {
+    this.props.adminLogin();
+  }
+
+  render() {
+    return <Loading />;
+  }
 }
 
-const mapStateToProps = state => {
-	return {
-		...state
-	};
-};
-
 export default connect(
-	mapStateToProps,
-	{ adminLogin }
+  null,
+  { adminLogin }
 )(AdminLogin);
