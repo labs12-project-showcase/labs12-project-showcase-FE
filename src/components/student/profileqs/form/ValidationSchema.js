2e30370c2442ffa25e4730bd828b4729bf4589bf
Clark <rclark010@gmail.com>
2019-05-24 08:29:56 -0600

    Adds youracclaim.com validation, adds https:// to URL validation message

--- src/components/student/profileqs/form/ValidationSchema.js
+++ src/components/student/profileqs/form/ValidationSchema.js
@@ -7,17 +7,21 @@ export const ProfileQsSchema = Yup.object().shape({
     .trim(),
   acclaim: Yup.string()
     .trim()
-    .url('Must be a valid URL'),
+    .url('Must be a valid URL, including https://...')
+    .matches(/^https?:\/\/www.youracclaim.com(.*)/i, {
+      excludeEmptyString: true,
+      message: `Must be from https://www.youracclaim.com/`
+    }),
   desired_title: Yup.string()
     .max(100, `Maximum 100 characters`)
     .trim(),
   github: Yup.string()
     .required('GitHub account link is required')
     .trim()
-    .url('Must be a valid URL'),
+    .url('Must be a valid URL, including https://...'),
   linkedin: Yup.string()
     .trim()
-    .url('Must be a valid URL'),
+    .url('Must be a valid URL, including https://...'),
   location: Yup.string().trim(),
   name: Yup.string()
     .max(100, `Maximum 100 characters`)
@@ -25,8 +29,8 @@ export const ProfileQsSchema = Yup.object().shape({
     .trim(),
   website: Yup.string()
     .trim()
-    .url('Must be a valid URL'),
+    .url('Must be a valid URL, including https://...'),
   twitter: Yup.string()
     .trim()
-    .url('Must be a valid URL')
+    .url('Must be a valid URL, including https://...')
 });
