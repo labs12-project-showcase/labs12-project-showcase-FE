bf8710677bb921c9f4630e3a4249ff6032f90f14
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-11 14:27:14 -0400

    Changes deployed url in ternary statement of 'frontendUrl' constant in urls.js to 'https://hirelambdastudents.com/'

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,9 +1,10 @@
 export const backendUrl =
-	process.env.NODE_ENV === 'development' && process.env.REACT_APP_TESTING_LOCAL_BACKEND === 'yespls'
-		? 'http://localhost:5000'
-		: 'https://halg-backend.herokuapp.com';
-		
+  process.env.NODE_ENV === 'development' &&
+  process.env.REACT_APP_TESTING_LOCAL_BACKEND === 'yespls'
+    ? 'http://localhost:5000'
+    : 'https://halg-backend.herokuapp.com';
+
 export const frontendUrl =
-	process.env.NODE_ENV === 'development'
-		? 'http://localhost:3000'
-		: 'https://lambdashowcase.netlify.com';
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'https://hirelambdastudents.com/';
